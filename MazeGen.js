a0b709ecf6b7f44766fb4c7cbdc8f2903b854aba
Thomas Young <thyo9470@colorado.edu>
2019-02-23 14:32:02 -0700

    Added layer

--- MazeGen.js
+++ MazeGen.js
@@ -4,26 +4,31 @@
 */
 var maze = [];
 
+var depth = 2;
 var height = 11;
 var width = 11;
 
 /*
   Setup
 */
-for(let y = 0; y < height; y++){
-  let row = [];
-  for(let x = 0; x < width; x++){
-    row.push(0); 
-  }
-  maze.push(row);
-}
-
-function print_maze(){
-  maze_out = [];
+for(let z = 0; z < depth; z++){
+  let layer = []
   for(let y = 0; y < height; y++){
-    maze_out.push( maze[y].join(" ") );
+    let row = [];
+    for(let x = 0; x < width; x++){
+      row.push(0); 
+    }
+    layer.push(row);
   }
-  console.log(maze_out.join("\n"));
+  maze.push(layer);
 }
 
-print_maze();
+function print_layer(layer){
+  layer_out = [];
+  for(let y = 0; y < height; y++){
+    layer_out.push( layer[y].join(" ") );
+  }
+  console.log(layer_out.join("\n"));
+}
+
+print_layer(maze[1])
