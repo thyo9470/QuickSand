11a2673812afc15d3544044c5f501d28380f2dfa
miyo6032 <miyo6032@colorado.edu>
2019-02-24 09:56:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, './')));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
+app.use('/', express.static(path.join(__dirname, './')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + 'index.html')));
 app.listen(port, () => console.log(`QuickSand is live on port ${port}!`));
